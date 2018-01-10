f506febee719158cffeb03bb826d2e758537949e
Kayla Webb <kaylarwebb@gmail.com>
2018-01-10 16:55:54 -0500

    Add smooth scrolling to the page

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 'use strict';
+/*global $ */
+
+
+function smoothScroll() {
+  $(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 800);
+  });
+}
+
+$(document).ready(() => {
+  smoothScroll();
+});
